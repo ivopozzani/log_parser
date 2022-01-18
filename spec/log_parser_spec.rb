b569c1c72f08c3711b1052f5de5217f45619b475
ivopozzani <ivo.pozzani@gmail.com>
2022-01-18 14:38:43 -0300

    refactor: code factored to match review

--- spec/log_parser_spec.rb
+++ spec/log_parser_spec.rb
@@ -1,13 +1,14 @@
 require 'log_parser'
-describe 'ReadFile' do
-  it '#prints first line' do
-        file = LogParser.new("games.log")
-        expect(file.read_first_line).to be_kind_of(String)
-  end
 
-  it '#checks file path' do
-        file = LogParser.new("games.log")
-        file.file_path = ''
-        expect { raise "File not found" }.to raise_error(RuntimeError, "File not found")
+describe 'ReadFile' do
+  describe '#read_first_line' do
+    it 'prints first line' do
+       file = LogParser.new("games.log")
+       expect(file.read_first_line).to eq("  0:00 ------------------------------------------------------------\n")      
+    end
+
+    it 'checks file path' do
+       expect { LogParser.new("games.txt") }.to raise_error(RuntimeError, "File not found")
+    end
   end
 end
