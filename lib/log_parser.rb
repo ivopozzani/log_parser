a0e2c17b73ef5234a2f7bca44726c2ea68fdb953
ivopozzani <ivo.pozzani@gmail.com>
2022-01-18 13:18:44 -0300

    refactor: code factored to match review

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -1,15 +1,15 @@
 class LogParser
-    attr_accessor :file_path
+  attr_accessor :file_path
     
-    def initialize(file_path)
-       @file_path = file_path
-     raise "File not found" unless File.exists?(@file_path)
-       @file = File.open(@file_path)       
-    end
+  def initialize(file_path)
+    @file_path = file_path
+   raise "File not found" unless File.exists?(@file_path)
+    @file = File.open(@file_path)       
+  end
     
-    def read_first_line
-        first_line = @file.readline
-        @file.close
-        first_line        
-    end       
+  def read_first_line
+    first_line = @file.readline
+    @file.close
+    first_line        
+  end       
 end
