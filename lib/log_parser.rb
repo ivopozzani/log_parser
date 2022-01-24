499d45d77967295b13c8e88be5ad806376109e3a
ivopozzani <ivo.pozzani@gmail.com>
2022-01-24 13:41:29 -0300

    refactor: rubocop corrections implemented

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -1,40 +1,41 @@
-require "json"
+require 'json'
 
 class LogParser
-  attr_accessor :file_path  
-    
+  attr_accessor :file_path
+
   def initialize(file_path)
     @file_path = file_path
-    raise "File not found" unless File.exists?(@file_path)
+    raise 'File not found' unless File.exist?(@file_path)
+
     @file = File.open(@file_path)
   end
-    
+
   def read_first_line
     first_line = @file.readline
     @file.close
-    first_line        
+    first_line
   end
 
-  def parse_file    
-    parse_json = JSON.generate({@file_path => {"lines" => line_counter, "players" => find_players}})
+  def parse_file
+    parse_json = JSON.generate({ @file_path => { 'lines' => line_counter, 'players' => find_players } })
     @file.close
     parse_json
   end
 
   private
-  
-  def line_counter    
+
+  def line_counter
     @file.readlines.length
   end
 
   def find_players
     players = []
-    for line in File.foreach(@file_path) do
-      if line.include?("ClientUser")
-        player_name = line.slice(/\\(\w+\s*)+/).delete_prefix("\\")
+    File.foreach(@file_path).each do |line|
+      if line.include?('ClientUser')
+        player_name = line.slice(/\\(\w+\s*)+/).delete_prefix('\\')
         players << player_name unless players.include?(player_name)
-      end          
+      end
     end
-    players  
-  end  
+    players
+  end
 end
--- spec/log_parser_spec.rb
+++ spec/log_parser_spec.rb
@@ -3,22 +3,22 @@ require 'log_parser'
 describe LogParser do
   describe '#initialize' do
     it 'checks file path' do
-      expect { LogParser.new("games.txt") }.to raise_error(RuntimeError, "File not found")
-   end
+      expect { LogParser.new('games.txt') }.to raise_error(RuntimeError, 'File not found')
+    end
   end
 
   describe '#read_first_line' do
     it 'prints first line' do
-       file = LogParser.new("./spec/fixtures/game_test.log")
-       expect(file.read_first_line).to eq("  0:00 ------------------------------------------------------------\n")      
+      file = LogParser.new('./spec/fixtures/game_test.log')
+      expect(file.read_first_line).to eq("  0:00 ------------------------------------------------------------\n")
     end
   end
 
   describe '#parse_file' do
-    let(:file) { LogParser.new("./spec/fixtures/game_test.log") }
-     
-    it 'returns JSON object' do 
-      expect(file.parse_file).to include("{\"./spec/fixtures/game_test.log\":{\"lines\":5,\"players\":[\"Isgalamido\",\"Dono da Bola\",\"Mocinha")
-    end    
+    let(:file) { LogParser.new('./spec/fixtures/game_test.log') }
+
+    it 'returns JSON object' do
+      expect(file.parse_file).to include('{"./spec/fixtures/game_test.log":{"lines":5,"players":["Isgalamido","Dono da Bola","Mocinha')
+    end
   end
 end
